d6946c933eafe03cbba6f00379ff0bd0e1e2b6c1
Kris Stern <krisastern@gmail.com>
2020-11-29 19:26:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def create_figure():
         img.save(imageBuffer, format="JPEG")
         img = imageBuffer.getvalue()
 
-        softmax_tensor = sess.graph.get_tensor_by_name('normalized_input_image_tensor:0')
+        softmax_tensor = sess.graph.get_tensor_by_name('normalized_input_image_tensor')
         predictions = sess.run(softmax_tensor,
                                {'DecodeJpeg/contents:0': img})
         predictions = np.squeeze(predictions)
