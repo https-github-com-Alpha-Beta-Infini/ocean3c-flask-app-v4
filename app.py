78199a6145b021ceff2d572df44c551660b5a1e8
Kris Stern <krisastern@gmail.com>
2020-11-30 15:04:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,14 +64,14 @@ def create_figure():
     # input_shape = input_details[0]['shape']
     input_data = tf.compat.v1.expand_dims(img_array, axis=0)
     # interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.set_tensor(input_details, input_data)
+    interpreter.set_tensor(input_details['index'][0], input_data)
 
     interpreter.invoke()
 
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
     # results = interpreter.get_tensor(output_details[0]['index'])
-    results = interpreter.get_tensor(output_details)
+    results = interpreter.get_tensor(output_details['index'][0])
     print(f'results: {results}')
     tf.compat.v1.reshape(results, [1, 2034, 4], name=None)
 
