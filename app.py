7b2570002e33aedb523e7e2ac24bd892845a7823
Kris Stern <krisastern@gmail.com>
2020-11-30 14:47:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def create_figure():
     # Get input and output tensors.
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
+    print(f'output_details: {output_details}')
 
     # Test the model on random input data.
     # input_shape = input_details[0]['shape']
@@ -69,7 +70,7 @@ def create_figure():
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
     results = interpreter.get_tensor(output_details[0]['index'])
-    print(results)
+    print(f'results: {results}')
     tf.compat.v1.reshape(results, [1, 2034, 4], name=None)
 
     # load annotations to decode classification result
