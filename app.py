d4dcbd3cfaa9c71091f38f7830deb0043ef1d840
Kris Stern <krisastern@gmail.com>
2020-11-30 14:43:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,7 @@ def create_figure():
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
     results = interpreter.get_tensor(output_details[0]['index'])
+    print(results)
     tf.compat.v1.reshape(results, [1, 2034, 4], name=None)
 
     # load annotations to decode classification result
