f35531ecc38b24ac0faca0a6a23e679a592b87b7
Kris Stern <krisastern@gmail.com>
2020-11-30 14:51:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ def create_figure():
 
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
-    results = interpreter.get_tensor(output_details[0]['index'])
+    # results = interpreter.get_tensor(output_details[0]['index'])
+    results = interpreter.get_tensor(output_details)
     print(f'results: {results}')
     tf.compat.v1.reshape(results, [1, 2034, 4], name=None)
 
